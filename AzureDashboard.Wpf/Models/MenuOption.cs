27de500497db0d38d3d82eed04dedd6e70de1609
Svend Tofte <stofte@gmail.com>
2018-06-17 16:04:28 +0200

    Wire pagemenu

---
+++ AzureDashboard.Wpf/Models/MenuOption.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureDashboard.Wpf.Models
+{
+    public class MenuOption
+    {
+        public string Name { get; set; }
+        public string Icon
+        {
+            get
+            {
+                switch (Kind)
+                {
+                    case MenuOptionKinds.Accounts: 
+                        return "AccountMultiple";
+                    case MenuOptionKinds.Dashboard:
+                        return "Apps";
+                    default:
+                        throw new ArgumentException("unknown kinds");
+                }
+            }
+        }
+        public MenuOptionKinds Kind { get; set; }
+    }
+}
--- AzureDashboard.Wpf/ViewModels/PageMenuViewModel.cs
+++ AzureDashboard.Wpf/ViewModels/PageMenuViewModel.cs
@@ -1,6 +1,8 @@
-﻿using Caliburn.Micro;
+﻿using AzureDashboard.Wpf.Models;
+using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,30 @@ namespace AzureDashboard.Wpf.ViewModels
 {
     public class PageMenuViewModel : Screen
     {
+        public Prop<MenuOption> SelectedMenu { get; set; }
+
+        public ObservableCollection<MenuOption> MenuOptions { get; set; }
+
+        public PageMenuViewModel()
+        {
+            var menuOpts = new MenuOption[]
+            {
+                new MenuOption
+                {
+                    Name = "Dashboard",
+                    Kind = MenuOptionKinds.Dashboard
+                },
+                new MenuOption
+                {
+                    Name = "Accounts",
+                    Kind = MenuOptionKinds.Accounts
+                },
+            };
+
+            SelectedMenu = new Prop<MenuOption>(menuOpts[0]);
+            MenuOptions = new ObservableCollection<MenuOption>();
+            MenuOptions.Add(menuOpts[0]);
+            MenuOptions.Add(menuOpts[1]);
+        }
     }
 }
