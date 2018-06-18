9e6f7e9f82b400c86646605d29d897253350634e
Svend Tofte <stofte@gmail.com>
2018-06-18 12:31:49 +0200

    Add sqlite

---
+++ AzureDashboard.Services/Storage/Database.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureDashboard.Services.Storage
+{
+    public class Database
+    {
+        
+    }
+}
